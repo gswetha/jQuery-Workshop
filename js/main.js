b74666cd9fad4c047795d41a5b393cd46c9029c7
G Swetha <gswetha@hotmail.com>
2012-09-24 11:49:48 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// console.log("main.js is loaded"); <!--write this to the console-->
+// $("img").width(200);
+//following function means: when the document(html doc) is ready, run this function.
+//always include this function and write all your code inside
+$(document).ready(function(){
+	$("img").on("mouseover", function(){
+		$("img").width(200);
+		$("h1").fadeOut();
+		$(".booboo").slideUp();
+		$("#rahul").hide();
+	});
+
+	$("img").mouseout(function(){
+		$("img").width(500);
+	});
+
+	// $("img").click(function(){
+	// 	$("img").width(200);
+	// });
+}); //always rmb to put ;
